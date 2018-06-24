2016b8dc84a9ec5d32d6790f14fc53ffaed30a98
mxm <fjp790308448@gmail.com>
2018-06-24 22:55:06 +0800

    16.推荐界面-自定义PrettyCell

--- DouYUZB/DouYUZB/Classes/Home/Controller/RecommendViewController.swift
+++ DouYUZB/DouYUZB/Classes/Home/Controller/RecommendViewController.swift
@@ -12,11 +12,15 @@ private let itemHorizontalMargin: CGFloat = 10
 
 private let itemWidth: CGFloat = (ScreenW - itemHorizontalMargin * 3 ) / 2
 
-private let itemHeight: CGFloat = itemWidth * 3 / 4
+private let normalItemHeight: CGFloat = itemWidth * 3 / 4
+
+private let prettyItemHeight: CGFloat = itemWidth * 4 / 3
 
 private let headerHeight: CGFloat = 50
 
 private let normalCellId = "normalCellId"
+
+private let prettyCellId = "prettyCellId"
 
 private let headerId = "headerId"
 
@@ -27,12 +31,13 @@ class RecommendViewController: UIViewController {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.minimumLineSpacing = 0
         flowLayout.minimumInteritemSpacing = itemHorizontalMargin
-        flowLayout.itemSize =  CGSize(width: itemWidth, height: itemHeight)
+        flowLayout.itemSize =  CGSize(width: itemWidth, height: normalItemHeight)
         flowLayout.headerReferenceSize = CGSize(width: ScreenW, height: headerHeight)
         flowLayout.sectionInset = UIEdgeInsets(top: 0, left: itemHorizontalMargin, bottom: 0, right: itemHorizontalMargin)
         
         let collectionView = UICollectionView(frame: (self?.view.bounds)!, collectionViewLayout: flowLayout)
         collectionView.dataSource = self
+        collectionView.delegate = self
         
         collectionView.backgroundColor = UIColor.white
         
@@ -40,6 +45,7 @@ class RecommendViewController: UIViewController {
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         
         collectionView.register(UINib(nibName: "HomeCollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: normalCellId)
+        collectionView.register(UINib(nibName: "HomeCollectionViewPrettyCell", bundle: nil), forCellWithReuseIdentifier: prettyCellId)
         collectionView.register(UINib(nibName: "HomeCollectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
         
         return collectionView
@@ -79,8 +85,24 @@ extension RecommendViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: normalCellId, for: indexPath)
+        var cell: UICollectionViewCell
+        
+        if indexPath.section == 1 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: prettyCellId, for: indexPath)
+        } else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: normalCellId, for: indexPath)
+        }
 
         return cell
+    }
+}
+
+extension RecommendViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 1 {
+            return CGSize(width: itemWidth, height: prettyItemHeight) 
+        } else {
+            return CGSize(width: itemWidth, height: normalItemHeight)
+        }
     }
 }
---
+++ DouYUZB/DouYUZB/Classes/Main/View/HomeCollectionViewPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeCollectionViewPrettyCell.swift
+//  DouYUZB
+//
+//  Created by mxm on 2018/6/24.
+//  Copyright © 2018年 mxm. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
